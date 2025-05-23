98133fd62e544eb3b8bdc90721fb615f0ee818cf
AndryuGC <andryupgc@gmail.com>
2025-05-23 08:10:13 -0600

    Segundo Parcial - Andry

--- src/main/java/ed/lab/E01MeetingRooms.java
+++ src/main/java/ed/lab/E01MeetingRooms.java
@@ -1,11 +1,21 @@
 package ed.lab;
 
-import java.util.List;
+import java.util.*;
 
 public class E01MeetingRooms {
 
     public int minMeetingRooms(List<MeetingInterval> meetingIntervals) {
-        return -1;
-    }
+        if (meetingIntervals == null || meetingIntervals.isEmpty()) return 0;
 
+        meetingIntervals.sort(Comparator.comparingInt(MeetingInterval::startTime));
+        PriorityQueue<Integer> tiempoFinal = new PriorityQueue<>();
+
+        for (MeetingInterval interval : meetingIntervals) {
+            if (!tiempoFinal.isEmpty() && interval.startTime() >= tiempoFinal.peek()) {
+                tiempoFinal.poll();
+            }
+            tiempoFinal.offer(interval.endTime());
+        }
+        return tiempoFinal.size();
+    }
 }
--- src/main/java/ed/lab/E02Logger.java
+++ src/main/java/ed/lab/E02Logger.java
@@ -1,12 +1,20 @@
 package ed.lab;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class E02Logger {
+    public final Map<String, Integer> messageTime;
 
     public E02Logger() {
+        this.messageTime = new HashMap<>();
     }
 
     public boolean shouldPrintMessage(int timestamp, String message) {
+        if (!messageTime.containsKey(message) || timestamp >= messageTime.get(message)) {
+            messageTime.put(message, timestamp + 10);
+            return true;
+        }
         return false;
-    }
-
+        }
 }
